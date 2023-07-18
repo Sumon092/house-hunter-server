c0088189f04f34336feb9e95ca8f4055c0f4da21
Sumon <m.sumon@outlook.com>
2023-07-19 00:07:39 +0600

    renter count and max limit fixed

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-// const config = require("./config/index.js");
+const config = require("./config/index.js");
 const app = require("./app.js");
 const port = 5000;
 
 async function main() {
   try {
-    // await mongoose.connect(config.database_url);
-    await mongoose.connect("mongodb://127.0.0.1:27017/house-hunter");
+    await mongoose.connect(config.database_url);
+    // await mongoose.connect("mongodb://127.0.0.1:27017/house-hunter");
     console.log("Connected to data base");
     app.listen(port, () => {
       console.log(`Hunter Server is running on port ${port}`);
